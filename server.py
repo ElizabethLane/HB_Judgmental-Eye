3a14b8e35b853e5e15e5d6d227d3e29f8aaf3b22
Hackbright Student <info@hackbrightacademy.com>
2016-08-04 17:53:42 -0700

    Finished part 2 up to USER DETAILS(not including).

--- server.py
+++ server.py
@@ -22,8 +22,11 @@ app.jinja_env.undefined = StrictUndefined
 @app.route('/')
 def index():
     """Homepage."""
+    #This returns none if the user is not in the browser session. Now in the 
+    #homepage.html we will show the sign up and log in button because satisfy the else. 
+    user = session.get("user")
 
-    return render_template("homepage.html")
+    return render_template("homepage.html",user=user)
 
 @app.route('/users')
 def user_list():
@@ -59,13 +62,59 @@ def register_process():
             #get the user_id from the user_id => user.user_id  
             
         else:
+            #This user is the user we plan to add to the database.
             user = User(email=email,password=password)
-            print user
+            #We are adding the user to the browser session to manage the buttons that
+            #are displayed (see / route, it gets the values for the key "user")
+            session["user"] = user.email
+            #Adding the user as an object to the db session.
             db.session.add(user)
+            #Committing the user to the database. 
             db.session.commit()
 
         return redirect("/")
 
+@app.route("/login", methods=["GET", "POST"])
+def login_process():
+
+    if request.method == "GET":
+        
+        return render_template("login.html")
+
+
+    if request.method == "POST":
+
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        user = User.query.filter(User.email == email).first()
+
+        #Checking if the user exists in the database.
+        if not user:
+            flash('Invalid Credentials')
+            return render_template("login.html")
+
+        #In the case that the user does exist, does the password they enter match?
+        elif user.password == password:
+            #telling the browser that a user is logged in.
+            session["user"] = user.user_id
+            flash('You were successfully logged in.')
+            return redirect("/")
+        
+        #If the password does not match that which is in the database, reload page 
+        #and flash invalid credentials. 
+        else:
+            flash('Invalid Credentials')
+            return render_template("login.html")
+
+@app.route('/logout')
+def logout_user():
+    """Log out the user and delete user from session."""
+
+    flash('You have successfully logged out.')
+    del session['user']
+
+    return redirect("/")
 
 
 if __name__ == "__main__":
